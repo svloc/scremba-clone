05316131375a3d8bfe7d3f8b0f46e8ae82b24b78
svloc <svloc39@gmail.com>
2026-06-20 21:21:36 +0530

    Upgrade Angular 14 to Angular 15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+    imports: [
+    NavbarComponent,
+    HomeComponent,
+    RouterOutlet
+  ]
 })
+
 export class AppComponent {
   title = 'comptask';
 }

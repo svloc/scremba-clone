1a17e962161839e9e8e2d71b8cdd81aad6cd6d78
svloc <svloc39@gmail.com>
2026-06-21 13:36:52 +0530

    Upgrade Angular 18 to Angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
+    templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
     imports: [
     NavbarComponent,
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  standalone: true,
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })

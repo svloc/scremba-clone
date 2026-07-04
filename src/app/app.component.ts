46a864f761e630e0c4e7fdfe5d6437a5edb2cd88
svloc <svloc39@gmail.com>
2026-07-04 14:37:50 +0530

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,22 @@ import { RouterOutlet } from '@angular/router';
   imports: [NavbarComponent, RouterOutlet]
 })
 export class AppComponent {
-  title = 'comptask';
+fireflies = Array.from({ length: 50 }, (_, i) => ({
+  id: i,
+  x1: this.random(-45, 45),
+  y1: this.random(-45, 45),
+  x2: this.random(-45, 45),
+  y2: this.random(-45, 45),
+  scale1: this.random(0.3, 1),
+  scale2: this.random(0.3, 1),
+  duration: this.random(15, 35),
+  rotate: this.random(6, 15),
+  flash: this.random(4, 8),
+  delay: this.random(0, 8)
+}));
+
+  private random(min: number, max: number): number {
+    return +(Math.random() * (max - min) + min).toFixed(2);
+  }
 }
 
